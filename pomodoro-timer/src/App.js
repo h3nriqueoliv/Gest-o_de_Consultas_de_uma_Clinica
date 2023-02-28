e89898eb27f1da24e090cdfca7b86ee218252328
h3nrique <henrique.o@ipvc.pt>
2023-02-28 13:57:50 +0000

    background-changes

--- pomodoro-timer/src/App.js
+++ pomodoro-timer/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar/NavaBar";
 import Hompg from "./Home/Homepg.js";
--- pomodoro-timer/src/Home/Homepg.js
+++ pomodoro-timer/src/Home/Homepg.js
@@ -4,21 +4,6 @@ import './Background.css'
 const Hompg = () => {
   return (
     <div className="content-home">
-      <div class="area" >
-        <tittle>Pomodoro Timer</tittle>
-            <ul class="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul>
-    </div >
     </div>
   );
 };
